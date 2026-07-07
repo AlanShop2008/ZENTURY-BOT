0849a79a1f8d6489814998822b3ad00b565e0b9e
Alan Martinez <alansnvgmx@gmail.com>
2026-07-07 14:50:36 -0600

    Add initial configuration for Zentury Bot

---
+++ config.js
@@ -0,0 +1,23 @@
+global.owner = [
+  ['5217715555998', 'Alan Shop', true]
+]
+
+global.mods = []
+global.prems = []
+
+global.botname = 'Zentury Bot'
+global.namebot = 'Zentury Bot'
+global.nameqr = 'Zentury Bot'
+global.author = 'ALAN SHOP'
+
+global.prefijo = '.'
+
+global.APIs = {}
+global.APIKeys = {}
+
+global.botNumber = ''
+
+global.wm = 'Zentury Bot'
+global.packname = 'Zentury Bot'
+global.stickpack = 'Zentury Bot'
+global.stickauth = 'ALAN SHOP'
